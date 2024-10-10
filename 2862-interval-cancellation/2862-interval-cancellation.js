0293a5d28b19ee23f3f9a44fc2fccf638e00c2e6
Hyunjoo Han <hyunjoo.han25@gmail.com>
2024-10-09 22:06:01 -0400

    Time: 66 ms (56.08%), Space: 49.4 MB (65.15%) - LeetHub

---
+++ 2862-interval-cancellation/2862-interval-cancellation.js
@@ -0,0 +1,50 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    const results = [{ time: 0, returned: fn(...args) }];
+
+    // Set an interval to repeatedly call the function
+    const intervalId = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        results.push({ time: elapsedTime, returned: fn(...args) });
+    }, t);
+
+    // Record the start time
+    const startTime = Date.now();
+
+    // Return the cancel function
+    return () => clearInterval(intervalId);
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)    
+ */
